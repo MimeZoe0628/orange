013b2ea7433c3cccb6c6dcd8c2a4e7c4424debd5
fastds <553563146@qq.com>
2018-06-25 15:16:50 +0800

    feat: 删除无用代码

--- Orange/ensembles/lightgbm.py
+++ Orange/ensembles/lightgbm.py
@@ -19,16 +19,11 @@ class SklLightGBMClassificationLearner(SklLearnerClassification):
     __wraps__ = lightgbm_ensemble.LGBMClassifier
     __returns__ = SklLightGBMClassifier
 
-    def __init__(self, base_estimator=None, n_estimators=50, learning_rate=1.,
-                 algorithm='SAMME.R', random_state=None, preprocessors=None):
-        from Orange.modelling import Fitter
-        # If fitter, get the appropriate Learner instance
-        if isinstance(base_estimator, Fitter):
-            base_estimator = base_estimator.get_learner(
-                base_estimator.CLASSIFICATION)
-        # If sklearn learner, get the underlying sklearn representation
-        if isinstance(base_estimator, SklLearner):
-            base_estimator = base_estimator.__wraps__(**base_estimator.params)
+    def __init__(self, boosting_type="gbdt", num_leaves=31, max_depth=-1, learning_rate=0.1,
+                 n_estimators=100, subsample_for_bin=200000, objective=None, class_weight=None,
+                 min_split_gain=0.0, min_child_weight=0.001, min_child_samples=20, sub_sample=1.0,
+                 subsample_freq=0, colsample_bytree=1.0, reg_alpha=0.0, reg_lambda=0.0,
+                 random_state=None, preprocessors=None):
         super().__init__(preprocessors=preprocessors)
         self.params = vars()
 
@@ -41,15 +36,10 @@ class SklLightGBMRegressionLearner(SklLearnerRegression):
     __wraps__ = lightgbm_ensemble.LGBMRegressor
     __returns__ = SklLightGBMRegressor
 
-    def __init__(self, base_estimator=None, n_estimators=50, learning_rate=1.,
-                 loss='linear', random_state=None, preprocessors=None):
-        from Orange.modelling import Fitter
-        # If fitter, get the appropriate Learner instance
-        if isinstance(base_estimator, Fitter):
-            base_estimator = base_estimator.get_learner(
-                base_estimator.REGRESSION)
-        # If sklearn learner, get the underlying sklearn representation
-        if isinstance(base_estimator, SklLearner):
-            base_estimator = base_estimator.__wraps__(**base_estimator.params)
+    def __init__(self, boosting_type="gbdt", num_leaves=31, max_depth=-1, learning_rate=0.1,
+                 n_estimators=100, subsample_for_bin=200000, objective=None, class_weight=None,
+                 min_split_gain=0.0, min_child_weight=0.001, min_child_samples=20, sub_sample=1.0,
+                 subsample_freq=0, colsample_bytree=1.0, reg_alpha=0.0, reg_lambda=0.0,
+                 random_state=None, preprocessors=None):
         super().__init__(preprocessors=preprocessors)
         self.params = vars()
